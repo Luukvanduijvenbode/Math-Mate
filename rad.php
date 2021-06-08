b13f012e879c5da7b73b506f366a5f426cd8da8e
Luukvanduijvenbode <74899086+Luukvanduijvenbode@users.noreply.github.com>
2021-06-08 11:39:21 +0200

    php code

---
+++ rad.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+if ($_SESSION['ingelogd'] != true) {
+  header("Location: login.php");
+}
+
+?>
+
+
+
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="language" content="NL">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Luuk van Duijvenbode">
+    <meta name="keywords" content="">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            </ul>
+            <li><a id="uitloggen" href="uitloggen.php">Uitloggen</a></li>
+            <li><a class="menu" href="homepage.php">Homepage</a></li>
+            <li><a class="menu" href="#">Over ons</a></li>
+            <li><a class="menu" href="rekenpagina1.php">Eenvoudige rekenpagina</a></li>
+            <li><a class="menu" href="#">Reeksen</a></li>
+            <li><a class="menu" href="#">Complexe rekenpagina</a></li>
+            <li><a class="menu" href="#">Rad van foortuin</a></li>
+            <li><a class="menu" href="#">Game pagina</a></li>
+            </ul>
+        </nav>
+    </header>
